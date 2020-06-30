13fd56e08af0c9208051b0769a7b24364b81769a
Lucas Boaventura <lucasxboaventura@hotmail.com>
2020-06-30 12:02:52 -0300

    Adding new page admin-profile

--- frontend-web/src/pages/Profile/index.tsx
+++ frontend-web/src/pages/Profile/index.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React from "react";
 
 const Profile = () => {
   return (
     <div>
       <h1 style={{ color: '#FFF' }}>Profileeeee</h1>
+      <button className="button">Voltar</button>
     </div>
   );
 };
